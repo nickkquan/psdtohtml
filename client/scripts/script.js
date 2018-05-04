4f833207a10ae3185948343b3e2665ff509eb853
Nick Quan <nickkquan@gmail.com>
2018-05-04 07:28:31 -0700

    converted js file to oop

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -1,33 +1,37 @@
-$(document).ready(initalizeWebsite);
+$(document).ready(() => site.initializeWebsite());
 
-function initalizeWebsite() {
-	initializeCarousel();
-	activateHamburgerMenu();
-	navBarTransparency();
+class Website {
+	initializeWebsite() {
+		this.initializeCarousel();
+		this.activateHamburgerMenu();
+		this.navBarTransparency();
+	}
+
+	initializeCarousel() {
+		$(".carousel__gallery").slick({
+			autoplay: true,
+			autoplaySpeed: 5000,
+			dots: true,
+			infinite: true,
+			mobileFirst: true
+		});
+	}
+
+	activateHamburgerMenu() {
+		$("#hamburger-menu").on("click", () => {
+			$(".navigation-bar").toggleClass("responsive");
+		});
+	}
+
+	navBarTransparency() {
+		$(window).scroll(() => {
+			if ($(window).scrollTop() >= 150) {
+				$(".navigation").css("background", "black");
+			} else {
+				$(".navigation").css("background", "rgba(0, 0, 0, 0.75)");
+			}
+		});
+	}
 }
 
-function initializeCarousel() {
-	$(".carousel__gallery").slick({
-		autoplay: true,
-		autoplaySpeed: 5000,
-		dots: true,
-		infinite: true,
-		mobileFirst: true
-	});
-}
-
-function activateHamburgerMenu() {
-	$("#hamburger-menu").on("click", function() {
-		$(".navigation-bar").toggleClass("responsive");
-	});
-}
-
-function navBarTransparency() {
-	$(window).scroll(function() {
-		if ($(window).scrollTop() >= 150) {
-			$(".navigation").css("background", "black");
-		} else {
-			$(".navigation").css("background", "rgba(0, 0, 0, 0.75)");
-		}
-	});
-}
+const site = new Website();
